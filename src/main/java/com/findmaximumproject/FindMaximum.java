80da8185f8767507e1d23aac2fd546b038c43d02
anamfazal <anamfazal94@gmail.com>
2020-10-27 10:49:59 +0530

    Added: functionality code and test cases to find maximum of 3 floats

--- src/main/java/com/findmaximumproject/FindMaximum.java
+++ src/main/java/com/findmaximumproject/FindMaximum.java
@@ -1,15 +1,21 @@
-/* Purpose: To find maximum among 3 integers*/
+/* Purpose: Add code to find maximum among 3 floats*/
 package com.findmaximumproject;
 
 public class FindMaximum {
-     Integer x , y , z;
-    public FindMaximum(Integer x , Integer y, Integer z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-    public Integer calculateMaximum() {
+
+    public Integer calculateMaximumOf3Integers(Integer x , Integer y , Integer z) {
         Integer max = x;
+        if(y.compareTo(max) > 0) {
+            max = y;
+        }
+        if(z.compareTo(max) > 0){
+            max = z;
+        }
+        return max;
+    }
+
+    public Float calculateMaximumOf3Floats(Float x , Float y , Float z) {
+        Float max = x;
         if(y.compareTo(max) > 0) {
             max = y;
         }
--- src/test/java/com/findmaximumproject/FindMaximumTest.java
+++ src/test/java/com/findmaximumproject/FindMaximumTest.java
@@ -7,20 +7,39 @@ public class FindMaximumTest {
 
     @Test
     public void given3Integers_WithMaxAtPosition1_ShouldReturnSameIntegerAsMaximum() {
-        FindMaximum findMaximum = new FindMaximum(6 , 5 ,2);
-        int maximum = findMaximum.calculateMaximum();
-        Assert.assertEquals(6 , maximum);
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximum = findMaximum.calculateMaximumOf3Integers(6 , 5 , 2 );
+        Assert.assertEquals(java.util.Optional.of(6), java.util.Optional.of(maximum));
     }
     @Test
     public void given3Integers_WithMaxAtPosition2_ShouldReturnSameIntegerAsMaximum() {
-        FindMaximum findMaximum = new FindMaximum(5 , 6 , 2);
-        int maximum = findMaximum.calculateMaximum();
-        Assert.assertEquals(6 , maximum);
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximum = findMaximum.calculateMaximumOf3Integers(5 , 6 , 2);
+        Assert.assertEquals(java.util.Optional.of(6), java.util.Optional.of(maximum));
     }
     @Test
     public void given3Integers_WithMaxAtPosition3_ShouldReturnSameIntegerAsMaximum() {
-        FindMaximum findMaximum = new FindMaximum(5 , 2 , 6);
-        int maximum = findMaximum.calculateMaximum();
-        Assert.assertEquals(6 , maximum);
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximum = findMaximum.calculateMaximumOf3Integers(5 , 2 , 6);
+        Assert.assertEquals(java.util.Optional.of(6), java.util.Optional.of(maximum));
+    }
+
+    @Test
+    public void given3Floats_WithMaxAtPosition1_ShouldReturnSameFloatAsMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float maximum = findMaximum.calculateMaximumOf3Floats(6.4f , 5.2f , 2.5f );
+        Assert.assertEquals(java.util.Optional.of(6.4f), java.util.Optional.of(maximum)) ;
+    }
+    @Test
+    public void given3Floats_WithMaxAtPosition2_ShouldReturnSameFloatAsMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float maximum = findMaximum.calculateMaximumOf3Floats(5.2f , 6.4f , 2.5f);
+        Assert.assertEquals(java.util.Optional.of(6.4f), java.util.Optional.of(maximum));
+    }
+    @Test
+    public void given3Floats_WithMaxAtPosition3_ShouldReturnSameFloatAsMaximum() {
+        FindMaximum findMaximum = new FindMaximum();
+        Float maximum = findMaximum.calculateMaximumOf3Floats(5.2f , 2.5f , 6.4f);
+        Assert.assertEquals(java.util.Optional.of(6.4f), java.util.Optional.of(maximum));
     }
 }
